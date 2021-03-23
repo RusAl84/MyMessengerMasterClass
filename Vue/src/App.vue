99179cbd9046e0ff596cfdeeac2de69e62c42d44
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-03-23 04:35:05 +0300

    Add files via upload

---
+++ Vue/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <v-main>
+      <ChatPage/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import ChatPage from './components/ChatPage';
+export default {
+  name: 'App',
+  components: {
+    ChatPage,
+  },
+  data: () => ({}),
+}
+</script>
